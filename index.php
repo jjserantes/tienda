6c8aa318568ed1b70eb2dcc69a71ed6461d09638
jjserantes <jserantes@gmail.com>
2025-01-27 17:30:55 +0100

    confg

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tienda</title>
+    <link rel="stylesheet" href="CSS/index_css.css">
 </head>
 <body>
     <form action="" method="post">
