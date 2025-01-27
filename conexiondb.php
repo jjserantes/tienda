673f2df3ae52010da55a18af0a34cbe8756a2b9e
jjserantes <jserantes@gmail.com>
2025-01-27 20:18:41 +0100

    trabajando en tienda

--- conexiondb.php
+++ conexiondb.php
@@ -1,17 +1,15 @@
 <?php
-include ("config.php");
-$host = HOST:
+include("config.php");
+$host = HOST; // o la IP de tu servidor MySQL
 $user = DB_USER;
 $password = DB_PASS;
 $database = DB_DATABASE;
-try {
+
     // Crear la conexión con PDO
     $conexion = new PDO("mysql:host=$host;dbname=$database", $user, $password);
     // Establecer el modo de error de PDO a excepción
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Conexión exitosa";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
+
 
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,57 @@
+<?php
+if (isset($_POST["username"])) {
+    try {
+        include("conexiondb.php");
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $sql = "SELECT * FROM usuarios WHERE username = :username";
+        $stm = $conexion->prepare($sql);
+        $stm->bindParam(":username", $username);
+        $stm->execute();
+        $row = $stm->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            if (password_verify($password, $row["password"])) {
+                session_start();
+                $_SESSION["username"] = $username;
+                header("Location: tienda");
+            } else {
+                $error = "Usuario o contraseña incorrectos";
+            }
+        } else {
+            $error = "Usuario o contraseña incorrectos";
+        }
+    } catch (Exception $e) {
+        $error="Error al iniciar sesión, contacte con el administrador";
+    }
+
+}
+
+
+?>
+
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
-    <link rel="stylesheet" href="CSS/index_css.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
+
 <body>
     <form action="" method="post">
-        <label for="username">Nombre de Usuario:</label>
-        <input type="text" name="username" id="username" required placeholder="Nombre de Usuario">
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" required placeholder="Contraseña">
-        <input type="submit" value="Iniciar Sesión">
-    </form>    
+        <h1>Iniciar sesión tienda</h1>
+        <label for="username">Nombre de usuario</label>
+        <input type="text" name="username" id="username" required placeholder="Username">
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" required placeholder="Password">
+        <input type="submit" value="Iniciar sesión">
+        <?php if (isset($error)) {
+            echo "<p>" . $error . "</p>";
+        }
+        ?>
+    </form>
 </body>
+
 </html>
