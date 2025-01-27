4be0132ba350bc59d279d775ae265e375014a0b7
jjserantes <jserantes@gmail.com>
2025-01-27 16:45:19 +0100

    Empezando

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="username">Nombre de Usuario:</label>
+        <input type="text" name="username" id="username" required placeholder="Nombre de Usuario">
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" id="password" required placeholder="Contraseña">
+        <input type="submit" value="Iniciar Sesión">
+    </form>    
+</body>
+</html>
